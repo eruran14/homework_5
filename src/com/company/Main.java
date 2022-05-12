792180f7d8c184458ea688ce95044f865a1ca15a
eruran14 <eruran14@gmail.com>
2022-05-12 16:53:23 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Boss b1 = new Boss();
+    b1.setHealth(1000);
+    b1.setDamage(150);
+    b1.setDefenceType("Magical");
+        System.out.println(b1.getHealth() + " " + b1.getDamage() + " " + b1.getDefenceType());
+    createHeroes();
+    }
+
+    public static Hero[] createHeroes(){
+        Hero h1 = new Hero(450, 50);
+        Hero h2 = new Hero(350, 35, "Telekinesis");
+        Hero h3 = new Hero(300, 40, "Invisibility");
+        Hero[] createdHeroes = {h1, h2, h3};
+        for (int i = 0; i < createdHeroes.length; i++) {
+            System.out.println(createdHeroes[i].getHealth() + " " + createdHeroes[i].getDamage() + " " + createdHeroes[i].getSuperpower());
+        }
+        return createdHeroes;
+    }
+}
